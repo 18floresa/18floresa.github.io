2f8c87f996bf80916aa9113a258ca8815662da91
AngelFlores8 <Floresa18@udayton.edu>
2025-06-26 12:10:16 -0400

    Raj Updated 'waph.html'

---
+++ echo.php
@@ -0,0 +1,20 @@
+<?php
+  echo htmlentities($_REQUEST['data']);
+?>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Form</title>
+</head>
+<body>
+    
+    <form method="POST" action="https://waph-hackathon.eastus.cloudapp.azure.com/xss/level2/echo.php">
+        <label for="data">Enter Data:</label>
+        <input type="text" id="input" name="input" required>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+</body>
+</html> -->
